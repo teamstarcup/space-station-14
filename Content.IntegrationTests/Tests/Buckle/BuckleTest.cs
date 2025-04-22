53cdbee8751b015fef05d5d11a6b261a3d3b1602
PrincessPengy <114524259+princesspengy@users.noreply.github.com>
2025-04-21 21:31:51 -0700

    fix buckle test and add missing comment

--- Content.IntegrationTests/Tests/Buckle/BuckleTest.cs
+++ Content.IntegrationTests/Tests/Buckle/BuckleTest.cs
@@ -313,7 +313,8 @@ namespace Content.IntegrationTests.Tests.Buckle
             await server.WaitAssertion(() =>
             {
                 // Still buckled
-                Assert.That(buckle.Buckled);
+                //Assert.That(buckle.Buckled); // goob edit
+                // he's not supposed to be buckled with the new falling down system
 
                 // Now with no item in any hand
                 foreach (var hand in hands.Hands.Values)
