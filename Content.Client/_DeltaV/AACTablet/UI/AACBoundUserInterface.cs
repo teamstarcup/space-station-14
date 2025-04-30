b1364acc224ef71bd4bdd8fea7f3a96e4e46b107
Princess Pengy <114524259+princesspengy@users.noreply.github.com>
2025-04-29 21:56:14 -0700

    feat: aac tablet and cane loadouts (#17)
    * delta-v: aac tablet
    
    * delta-v: aac additions
    
    * delta-v: fix aac tablets not working on radio
    
    * delta-v: fix aac tablet with voice mask
    
    * delta-v: add canes to loadouts
    
    * delta-v: aac tablet refactor
    
    * delta-v: new aac phrases
    
    * delta-v: aac search tab and new ui
    
    * delta-v: aac phrase combining
    
    * aac tablet cleanup
    
    * delta-v: aac tablet
    
    * delta-v: aac additions
    
    * delta-v: fix aac tablets not working on radio
    
    * delta-v: fix aac tablet with voice mask
    
    * delta-v: add canes to loadouts
    
    * delta-v: aac tablet refactor
    
    * delta-v: new aac phrases
    
    * delta-v: aac search tab and new ui
    
    * delta-v: aac phrase combining
    
    * aac tablet cleanup
    
    * namespace refactor
    
    * strip delta exclusive phrases
    
    * added missing localization phrases
    
    * tweak: disable deathmatch, enable greenshift and extended (#16)
    
    * character records and height
    
    * embiggening the namespaces
    
    * character records and height
    
    * embiggening the namespaces
    
    * character records and height
    
    * embiggening the namespaces
    
    * delta species scales
    
    * delta-v: aac tablet
    
    * delta-v: aac additions
    
    * delta-v: aac tablet refactor
    
    * delta-v: new aac phrases
    
    * localization fixes
    
    * remove duplicate id
    
    ---------
    
    Co-authored-by: portfiend <109661617+portfiend@users.noreply.github.com>
    Co-authored-by: Milon <plmilonpl@gmail.com>
    Co-authored-by: ConstantlyConfused <67894661+Eternally-Confused@users.noreply.github.com>
    Co-authored-by: Milon <milonpl.git@proton.me>
    Co-authored-by: keekee38 <iamabanana372456@gmail.com>
    Co-authored-by: Oxdeception <30361731+Oxdeception@users.noreply.github.com>
    Co-authored-by: deltanedas <39013340+deltanedas@users.noreply.github.com>
    Co-authored-by: little-meow-meow <204685920+little-meow-meow@users.noreply.github.com>
    Co-authored-by: Lyndomen <49795619+Lyndomen@users.noreply.github.com>

---
+++ Content.Client/_DeltaV/AACTablet/UI/AACBoundUserInterface.cs
@@ -0,0 +1,29 @@
+using Content.Shared._DeltaV.AACTablet;
+using Content.Shared._DeltaV.QuickPhrase;
+using Robust.Shared.Prototypes;
+using Robust.Client.UserInterface;
+
+namespace Content.Client._DeltaV.AACTablet.UI;
+
+public sealed class AACBoundUserInterface : BoundUserInterface
+{
+    [ViewVariables]
+    private AACWindow? _window;
+
+    public AACBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+        _window?.Close();
+        _window = this.CreateWindow<AACWindow>();
+        _window.PhraseButtonPressed += OnPhraseButtonPressed;
+    }
+
+    private void OnPhraseButtonPressed(List<ProtoId<QuickPhrasePrototype>> phraseId)
+    {
+        SendMessage(new AACTabletSendPhraseMessage(phraseId));
+    }
+}
