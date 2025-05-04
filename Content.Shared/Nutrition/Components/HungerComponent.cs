a0e01870d9457ba3186c21efb43459cff93af168
PrincessPengy <114524259+princesspengy@users.noreply.github.com>
2025-05-04 01:29:07 -0700

    number tweaks

--- Content.Shared/Nutrition/Components/HungerComponent.cs
+++ Content.Shared/Nutrition/Components/HungerComponent.cs
@@ -34,7 +34,7 @@ public sealed partial class HungerComponent : Component
     /// </summary>
     /// <remarks>Any time this is modified, <see cref="HungerSystem.SetAuthoritativeHungerValue"/> should be called.</remarks>
     [DataField("baseDecayRate"), ViewVariables(VVAccess.ReadWrite)]
-    public float BaseDecayRate = 0.035f; // DeltaV: changed from 0.01666666666f
+    public float BaseDecayRate = 0.025f; // starcup: changed from 0.01666666666f (150% of standard)
 
     /// <summary>
     /// The actual amount at which <see cref="LastAuthoritativeHungerValue"/> decays.
